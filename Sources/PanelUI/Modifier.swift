d1c88ba3d196aac0d2054d9e2362d8c4cea0ba17
leoMehlig <leo@leomehlig.com>
2021-04-18 15:59:30 +0200

    Update

--- Sources/PanelUI/Modifier.swift
+++ Sources/PanelUI/Modifier.swift
@@ -1,12 +1,23 @@
 import SwiftUI
 
-struct PanelModifier<Body: View>: ViewModifier {
+public struct PanelModifier<Body: View>: ViewModifier {
+
     @Binding var isPresented: Bool
     let body: () -> Body
 
     @State private var state: PanelState = .init()
 
-    func body(content: Content) -> some View {
+    public init(isPresented: Binding<Bool>, body: @escaping () -> Body) {
+        self._isPresented = isPresented
+        self.body = body
+    }
+
+    public init<Item: Identifiable>(item: Binding<Item?>, body: @escaping (Item) -> Body) {
+        let binding = Binding(get: { item.wrappedValue != nil }, set: { if !$0 { item.wrappedValue = nil } })
+        self.init(isPresented: binding, body: { body(item.wrappedValue!) })
+    }
+
+    public func body(content: Content) -> some View {
         content
             .accessibility(hidden: self.isPresented && self.state.state == .expanded)
             .overlay(Panel(state: self.binding, content: self.body))
@@ -33,8 +44,8 @@ public extension View {
     func panel<Item: Identifiable, Content: View>(item: Binding<Item?>,
                                                   @ViewBuilder content: @escaping (Item) -> Content)
     -> some View {
-        let binding = Binding(get: { item.wrappedValue != nil }, set: { if !$0 { item.wrappedValue = nil } })
-        return self.modifier(PanelModifier(isPresented: binding,
-                                           body: { content(item.wrappedValue!) }))
+
+        return self.modifier(PanelModifier(item: item,
+                                           body: content))
     }
 }
